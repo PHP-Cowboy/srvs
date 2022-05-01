5c99c445dab5fccd03ab98c815becb59cf26ce98
chenjie <657499186@qq.com>
2022-05-01 16:26:02 +0800

    docker install shell

---
+++ docker.sh
@@ -0,0 +1,22 @@
+echo start...
+
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+
+systemctl start docker #启动docker
+systemctl enable docker
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://fydfkad9.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-uname -s-uname -m > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose -v
+
+echo end
