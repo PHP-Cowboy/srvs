ecd68e4a1cfdf9c9f73e519ed26ea54440eea3f1
chenjie <657499186@qq.com>
2022-08-28 03:03:19 +0800

    mysql8

---
+++ shell/mysql8.sh
@@ -0,0 +1,30 @@
+cd /
+
+mkdir -p mysql/conf
+
+mkdir -p mysql/data
+
+mkdir -p mysql/logs
+
+docker pull mysql:8.0
+
+docker run -d \
+-p 3305:3306 \
+-e MYSQL_ROOT_PASSWORD=123456 \
+--name mysql \
+--restart=always \
+-v /mysql/data:/var/lib/mysql \
+-v /mysql/conf:/etc/mysql \
+-v /mysql/mysql-files:/var/lib/mysql-files/ \
+-v /mysql/logs:/logs \
+mysql
+
+docker exec -it mysql /bin/bash
+
+mysql -uroot -p123456
+
+use mysql
+
+alter user 'root'@'localhost' identified by 'root';
+
+FLUSH PRIVILEGES;
