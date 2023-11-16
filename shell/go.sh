17a1bacf392bc42bf0c6446a902ec55f891814f1
PHP-Cowboy <657499186@qq.com>
2023-11-16 10:14:33 +0800

    Update go.sh

--- shell/go.sh
+++ shell/go.sh
@@ -2,17 +2,17 @@ echo "start..."
 
 cd /usr/local
 
-if  [ -e /usr/local/go1.18.1.linux-amd64.tar.gz ]; then   #这里是判断语句，-e表示进行比较结果为真则存在
+if  [ -e /usr/local/go1.21.0.linux-amd64.tar.gz ]; then   #这里是判断语句，-e表示进行比较结果为真则存在
 
 echo "文件存在"
 
 else
 
-wget https://studygolang.com/dl/golang/go1.18.1.linux-amd64.tar.gz
+wget https://studygolang.com/dl/golang/go1.21.0.linux-amd64.tar.gz
 
 fi
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 
 cd - #返回上一次目录
 
